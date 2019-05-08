d55cb0faecee04d4d0968b59ccc40bbb9ec03ccc
Robin Métral <robin@metral.ch>
2019-05-08 15:27:48 +0200

    Refactor fetch messages into new method

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,24 @@ import Join from "./Join";
 import Chat from "./Chat";
 
 class App extends Component {
+  // initialize state
   state = {
     author: undefined,
     messages: {}
   };
 
   componentDidMount() {
-    // check if author exists in localStorage
+    // get returning author from localStorage
     if (localStorage.getItem("author")) {
       this.setState({ author: localStorage.getItem("author") });
     }
+    // fetch messages
+    this.fetchMessages();
+  }
 
+  // hit API to fetch messages
+  fetchMessages = () => {
+    console.log("Fetching messages...");
     // fetch messages
     fetch(`https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/?token=`)
       .then(response => {
@@ -25,17 +32,19 @@ class App extends Component {
           messages: json
         });
       });
-  }
+  };
 
+  // join the chat by adding author to state
   joinChat = (author, remember) => {
     this.setState({ author });
+    // save to localStorage if desired
     if (remember) {
       localStorage.setItem("author", author);
     }
   };
 
+  // leave the chat by removing author from state
   leaveChat = () => {
-    // remove author from state
     this.setState({ author: undefined });
     // clear localStorage
     localStorage.clear();
