0b6298e375666d361b0019c6422c5a3e717d7ae2
Robin Métral <robin@metral.ch>
2019-05-11 16:58:13 +0200

    Rename and style JoinChat form

---
+++ src/components/JoinChat.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import IconButton from "./IconButton";
+import { ReactComponent as SignIn } from "../assets/sign-in.svg";
+
+const Form = styled.form`
+  margin: 40vh 2rem 0 2rem;
+  display: flex;
+  align-items: center;
+`;
+
+const TextInput = styled.input`
+  /* reset */
+  appearance: none;
+  font-family: inherit;
+  font-size: 100%;
+  box-sizing: border-box;
+  margin: 0;
+  /* style */
+  border: ${({ theme }) => theme.border};
+  border-radius: ${({ theme }) => theme.borderradius};
+  background-color: ${({ theme }) => theme.box};
+  color: ${({ theme }) => theme.text};
+  padding: 1rem 2rem;
+`;
+
+const Checkbox = styled.input`
+  /* reset */
+  appearance: none;
+  font-family: inherit;
+  font-size: 100%;
+  /* style */
+  width: 2rem;
+  height: 2rem;
+  font-size: 2rem;
+  border: ${({ theme }) => theme.border};
+  border-radius: 2px;
+  background-color: ${({ theme }) => theme.box};
+  color: ${({ theme }) => theme.text};
+  margin: 0 0.5rem;
+  /* style and position checked */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:checked::before {
+    content: "×";
+  }
+`;
+
+class JoinChat extends Component {
+  state = {
+    author: "",
+    remember: false
+  };
+
+  handleChange = event => {
+    const { name } = event.target;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.joinChat(this.state.author, this.state.remember);
+  };
+
+  render() {
+    return (
+      <>
+        <Form onSubmit={this.handleSubmit}>
+          <div
+            style={{ display: "flex", flexDirection: "column", width: "100%" }}
+          >
+            <TextInput
+              style={{ flex: "3" }}
+              required
+              type="text"
+              name="author"
+              value={this.state.author}
+              onChange={this.handleChange}
+              placeholder="Username"
+            />
+            <label
+              htmlFor="remember"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "1rem"
+              }}
+            >
+              <Checkbox
+                type="checkbox"
+                id="remember"
+                name="remember"
+                checked={this.state.remember}
+                onChange={this.handleChange}
+              />
+              Remember me
+            </label>
+          </div>
+          <IconButton type="submit" icon={SignIn} />
+        </Form>
+      </>
+    );
+  }
+}
+
+export default JoinChat;
