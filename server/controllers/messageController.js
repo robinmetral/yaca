c93896003e80c48271ca4e0e624bd293c982fe9d
Robin Métral <robin@metral.ch>
2019-05-30 15:04:40 +0200

    Send back errors from message controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,16 +1,16 @@
 import Message from "../models/message";
 
 export const message_get = (req, res, next) => {
-  Message.find({}, "user message timestamp").exec((err, message_list) => {
-    if (err) {
-      return next(err);
+  Message.find({}, "user message timestamp").exec((error, messages) => {
+    if (error) {
+      res.send({ error });
+    } else {
+      res.send(messages);
     }
-    // render
-    res.render("message_list", { title: "Messages", message_list });
   });
 };
 
 // create message
 export const message_post = (req, res) => {
-  res.send("Not implemented yet");
+  res.send({ error: "Not implemented yet" });
 };
