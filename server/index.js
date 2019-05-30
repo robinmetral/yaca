ea97f836a29dd6706bce61576e7b044b2e49466c
Robin Métral <robin@metral.ch>
2019-05-30 15:00:27 +0200

    Remove test data

--- server/index.js
+++ server/index.js
@@ -13,15 +13,6 @@ mongoose.connect(process.env.MONGODB_ENDPOINT, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// todo fetch data from mongodb
-const response = [
-  {
-    author: "robin",
-    message: "hey",
-    timestamp: 1559218918716
-  }
-];
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
