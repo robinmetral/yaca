432f798d03ec31a46226eab0615319608214341c
Robin Métral <robin@metral.ch>
2019-05-30 14:23:54 +0200

    Add test message to server response

--- server/index.js
+++ server/index.js
@@ -5,9 +5,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // todo fetch data from mongodb
-const response = {
-  message: "hey"
-};
+const response = [
+  {
+    author: "robin",
+    message: "hey",
+    timestamp: 1559218918716
+  }
+];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
